5596c8c1b03a856e73adcea12ad9fbb58e43ae8d
Joshua Schmitt <jqshuv@gmail.com>
2022-08-14 17:28:37 +0200

    Add credits

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/tawesoft/golib/v2/dialog"
 
 func main() {
-	err := dialog.Info("This is an example Programm.")
+	err := dialog.Info("This is an example Programm. \n Made by @jqhuv")
 	if err != nil {
 		return
 	}
